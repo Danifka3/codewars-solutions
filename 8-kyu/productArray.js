868325db68b38fdd2e837cf23bf20552b7720fe0
Danifka3 <danilagromyko24@gmail.com>
2023-03-16 15:38:06 +0300

    productArray

---
+++ 8-kyu/productArray.js
@@ -0,0 +1,11 @@
+function productArray(numbers){
+  const arrOfMultiples = new Array(numbers.length);
+  const multiply = numbers.reduce((multiple, item) => multiple * item, 1)
+  arrOfMultiples.fill(multiply);
+  return arrOfMultiples.map((item, index) => item / numbers[index])
+}
+
+// console.log(productArray([12,20]));
+// console.log(productArray([3,27,4,2]));
+// console.log(productArray([13,10,5,2,9]));
+// console.log(productArray([16,17,4,3,5,2]));
