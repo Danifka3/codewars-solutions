a6cfd14049e0da4c2d5e6e1334f260397059a302
Danifka3 <danilagromyko24@gmail.com>
2023-03-21 19:40:32 +0300

    countIslands

---
+++ 6-kyu/countIslands.js
@@ -0,0 +1,35 @@
+function countIslands(image){
+  let counter = 0;
+  for (let i = 0; i < image.length; i++) {
+    for (let j = 0; j < image[0].length; j++) {
+      if (image[i][j] === 1){
+        counter += 1;
+        image[i][j] = 0;
+        makeIslandZero(image,i,j)
+      }
+    }
+  }
+  return counter;
+}
+
+function makeIslandZero(image,x,y){
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      if (x+i < image.length && x+i >= 0 && y+j < image[0].length && y+j >= 0){
+        if (image[x+i][y+j] === 1){
+          image[Math.abs(x+i)][Math.abs(y+j)] = 0;
+          makeIslandZero(image,Math.abs(x+i),Math.abs(y+j));
+        }
+      }
+    }
+  }
+}
+
+const image = [
+  [1,0,0,0,1],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [1,0,0,0,1],
+];
+
+console.log(countIslands(image));
